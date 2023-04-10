40831139ad8282e64cde1a3e15c0ef6f8105c3fa
InLas <inlas645@mail.ru>
2023-04-10 20:40:13 +0300

    upload-form-validation fix

--- js/upload-form-validation.js
+++ js/upload-form-validation.js
@@ -3,48 +3,67 @@ import { sentData } from './api.js';
 
 const form = document.querySelector('.img-upload__form');
 
-const getTagsErrorMessage = (value) => {
-  const TAGS = value.split(' ');
-  const UNIQUE_TAGS = TAGS.map((tag) => tag.toLowerCase());
-
-  if (TAGS.length > 5) {
-    return 'Не больше 5 тегов';
-  }
-
-  if (new Set(UNIQUE_TAGS).size !== UNIQUE_TAGS.length) {
-    return 'Теги должны быть уникальны';
-  }
-
-  for (const TAG of TAGS) {
-    const ERROR = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/.test(TAG);
-    if (!ERROR) {
-      return 'Тег начинается с решетки и имеет максимальную длинну 20 символов (включая решетку), строка после решётки должна состоять из букв и чисел, теги указываются через пробел';
-    }
-  }
+const MAX_TAGS_LENGTH = 5;
+const TAG_VALIDATION = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/;
+const Errors = {
+  LENGTH_ERROR: 'Не больше 5 тегов',
+  UNIQUE_ERROR: 'Теги должны быть уникальны',
+  VALIDATE_ERROR: 'Тег начинается с решетки и имеет максимальную длинну 20 символов (включая решетку), строка после решётки должна состоять из букв и чисел, теги указываются через пробел'
 };
 
-const tagsCheck = (value) => {
+const checkTagsLength = (value) => {
   if (value !== '') {
     const TAGS = value.split(' ');
-    const UNIQUE_TAGS = TAGS.map((tag) => tag.toLowerCase());
 
-    if (TAGS.length > 5) {
+    if (TAGS.length > MAX_TAGS_LENGTH) {
       return false;
     }
+  }
+
+  return true;
+};
+
+const checkTagsUnique = (value) => {
+  if (value.length > 1) {
+    const TAGS = value.split(' ');
+    const UNIQUE_TAGS = TAGS.map((tag) => tag.toLowerCase());
 
     if (new Set(UNIQUE_TAGS).size !== UNIQUE_TAGS.length) {
       return false;
     }
+  }
+
+  return true;
+};
+
+const checkTagsValidation = (value) => {
+  if (value !== '') {
+    const TAGS = value.split(' ');
 
     for (const TAG of TAGS) {
-      const ERROR = /^#[A-Za-zА-Яа-яЁё0-9]{1,19}$/.test(TAG);
-      if (!ERROR) {
+      if (!TAG_VALIDATION.test(TAG)) {
         return false;
       }
     }
   }
 
   return true;
+};
+
+const tagsCheck = (value) => checkTagsLength(value) && checkTagsUnique(value) && checkTagsValidation(value);
+
+const getTagsErrorMessage = (value) => {
+  if (!checkTagsLength(value)) {
+    return Errors.LENGTH_ERROR;
+  }
+
+  if (!checkTagsUnique(value)) {
+    return Errors.UNIQUE_ERROR;
+  }
+
+  if (!checkTagsValidation(value)) {
+    return Errors.VALIDATE_ERROR;
+  }
 };
 
 const PRISTINE = new Pristine(form, {
